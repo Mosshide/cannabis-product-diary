f58695443cd8222e4e5d1f06c26eb261fc4af6b3
wsushinsky <wyattsushi@gmail.com>
2021-06-01 16:08:25 -0700

    setup to view initial home page
    Signed-off-by: wsushinsky <wyattsushi@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+require("dotenv").config();
 
-app.listen(PORT, () => {
-    console.log(`Listening for client requests on port ${PORT}`);
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get("/", async function(req, res) {
+    res.render("index");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening for client requests on port ${process.env.PORT}`);
 })
