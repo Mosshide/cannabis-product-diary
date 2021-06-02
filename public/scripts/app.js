8ee1805f4656ff0902840f2fb676a9a2bb91510a
wsushinsky <wyattsushi@gmail.com>
2021-06-02 16:09:21 -0700

    form slides out now
    Signed-off-by: wsushinsky <wyattsushi@gmail.com>

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,23 @@
 $("#new").on("click", function() {
     $(".side").toggleClass("side-out");
-    console.log("sdg");
-})
+    $(".gallery").toggleClass("gallery-compact");
+    $("#new").css("display", "none");
+    $("#accept").css("display", "initial");
+    $("#cancel").css("display", "initial");
+});
+
+$("#accept").on("click", function() {
+    $(".side").toggleClass("side-out");
+    $(".gallery").toggleClass("gallery-compact");
+    $("#new").css("display", "initial");
+    $("#accept").css("display", "none");
+    $("#cancel").css("display", "none");
+});
+
+$("#cancel").on("click", function() {
+    $(".side").toggleClass("side-out");
+    $(".gallery").toggleClass("gallery-compact");
+    $("#new").css("display", "initial");
+    $("#accept").css("display", "none");
+    $("#cancel").css("display", "none");
+});
